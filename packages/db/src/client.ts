b1dd2ae1d016f9c9c6dd5b990dd18d1e9a5b7a28
Rohan Sheth <rohanjaiminsheth@gmail.com>
2026-06-01 19:00:23 -0700

    formatting

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,11 +2,11 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-const databaseUrl = process.env.databaseUrl
+const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
     throw new Error("DATABASE_URL is not set");
-  }
+}
 
 const sql = neon(databaseUrl);
 
