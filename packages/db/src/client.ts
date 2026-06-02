f8b9ca4021f7bb4bd663103d64ae050867151648
Rohan Sheth <rohanjaiminsheth@gmail.com>
2026-06-01 18:59:13 -0700

    added client

---
+++ packages/db/src/client.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema";
+
+const databaseUrl = process.env.databaseUrl
+
+if (!databaseUrl) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+const sql = neon(databaseUrl);
+
+export const db = drizzle(sql, { schema });
