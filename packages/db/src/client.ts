3243e0addcadcb321f7febac45bcf2e3277f0520
Rohan Sheth <rohanjaiminsheth@gmail.com>
2026-06-22 04:12:57 -0700

    vercel2

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
+import * as schema from "./schema.js";
 
 const databaseUrl = process.env.DATABASE_URL;
 
